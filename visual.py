470bae799ae2760596d3991b260fa1825ca82c0e
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2020-09-30 17:02:08 +0200

    work work

--- visual.py
+++ visual.py
@@ -3,20 +3,22 @@
 import argparse, logging, math, os
 import numpy as np
 import shutil
-from ctypes import *
+#from ctypes import *
 #import mesh_io
 import meshio #dependency for easy reading of vtu files
 import json
 import precice
 import xml.etree.ElementTree as ET #dependency to parse ParaView pvd file 
+import collections
 
 
 '''
 Run the script with 
-	python3 visual.py data-0.vtk
-
-
+	python3 visual.py case1/case1_single_tracer_fracture.pvd case1/precice-config.xml
 '''
+
+TIME_EPS = 1e-10
+DataSet = collections.namedtuple("DataSet", [ "t", "path" ])
 
 class Mesh:
 	"""
@@ -49,6 +51,8 @@ class DumuxMesh:
 	"""
 	"""
 	def __init__(self, points = None ):
+
+
 		if points is not None:
 			self.points = points
 		else:
@@ -58,6 +62,8 @@ class DumuxMesh:
 
 		self.mesh_id = -1
 
+		self.mesh_name = "unnamed"
+
 	def __str__(self):
 		numbers = ""
 		if len( self.points) > 4:
@@ -66,18 +72,11 @@ class DumuxMesh:
 			for p in self.points:
 				numbers += "{}".format( p )
 
-		return "DuMuX mesh stats:\n  {} points\n  Coordinates: {}\n".format( len( self.points), numbers ) )
+		return "DuMuX mesh stats:\n  {} points\n  Coordinates: {}\n".format( len( self.points), numbers ) 
 
 
-def read_mesh(filename, tag, datadim=1):
-	points, cells, cell_types, pointdata = mesh_io.read_mesh(filename, tag)
-	#print("Points: ", len(points))
-	#print("Point data: ", pointdata)
-	return Mesh(points, cells, cell_types, pointdata)
 
 
-cell_type = 'quad'
-data_label = 'x^tracer_0'
 
 def create_file_list( path_to_pvd ):
 	base_path = path_to_pvd.rsplit("/", maxsplit=1)[0]
@@ -94,16 +93,18 @@ def create_file_list( path_to_pvd ):
 	file_list = []
 	for dataset in root.iter("DataSet"):
 		print( dataset.attrib["file"] )
-		file_list.append( base_path + dataset.attrib["file"] )
+		#file_list.append( base_path + dataset.attrib["file"] )
+		file_list.append( DataSet( dataset.attrib["timestep"], base_path + dataset.attrib["file"]) )
 
+	print( file_list )
 	return file_list
 
 
-def extract_mesh( file_list ):
+def extract_mesh( file_list, cell_type = "quad" ):
 
 	print( "Extracting mesh from first vtu file!")
 
-	paraview_mesh = meshio.read( file_list[0] )
+	paraview_mesh = meshio.read( file_list[0].path )
 	print( paraview_mesh )
 
 	vis_points = np.zeros(( len(paraview_mesh.cells[cell_type]), 3)) 
@@ -121,6 +122,14 @@ def extract_mesh( file_list ):
 	
 	return DumuxMesh( vis_points )
 
+def extract_cell_data( file_name, data_label, cell_type = "quad" ):
+
+	#print( "Extracting mesh from first vtu file!")
+	vtu_data = meshio.read( file_name.path )
+	#print( paraview_mesh )
+	print( vtu_data.cell_data[cell_type][data_label] )
+
+	return vtu_data.cell_data[cell_type][data_label]
 
 
 def main():
@@ -128,81 +137,26 @@ def main():
 	vtu_file_list = create_file_list( args.pvd_filename )
 	print( vtu_file_list )
 	my_mesh = extract_mesh( vtu_file_list )
+	my_mesh.mesh_name = "DumuxMesh"
 	print( my_mesh )
-	#base_path = args.pvd_filename.rsplit("/", maxsplit=1)[0]
-	#if ( base_path == args.pvd_filename ):
-	#	base_path = ""
-	#print( "Path to VTK files: \"{}\"".format(base_path) )
+
+	#print( extract_cell_data( vtu_file_list[0], "x^tracer_0" ) )
+
 
 	print("Starting visualization coupling...")
 
-	configuration_file_name = "precice-config.xml"
+	configuration_file_name = args.precice_config #"precice-config.xml"
 	participant_name = "Dumux"
-	mesh_name = "DumuxMesh"
+	#mesh_name = "DumuxMesh"
 	
-	#paraview_mesh = meshio.read("case1/case1_single_tracer_fracture-00000.vtu")
-
-
-	#print( "General info:\n  {}".format(paraview_mesh) )
-	#print( "Cells info:\n {}".format(paraview_mesh.cells) )
-
-	#vis_points = np.zeros(( len(paraview_mesh.cells[cell_type]), 3)) 
-
-	#print( vis_points )
-	#for idx, point_list in zip( range(0, len(vis_points)), paraview_mesh.cells[cell_type] ):
-	#for celllist in paraview_mesh.cells["quad"]:
-	#	tmp = np.zeros((1,3))
-#		for cell in point_list:
-#			for point in paraview_mesh.points[ cell ]:
-		#print( dataset[0] )
-			#print( cell )
-				#print( point )
-				#print( paraview_mesh.points[ cell ] )
-				#print( paraview_mesh.points[ point_id ] )
-#				tmp = np.add(tmp, point )
-#		vis_points[idx] = tmp
-			#paraview_mesh.points[ dataset[] ]
-		#print( "tmp {}".format(tmp) )
-		#print( "Cell center: {}".format( np.divide(tmp, 4) ) )
-	
-
-	
-	#my_mesh = DumuxMesh( vis_points )
-
-	#print( vis_points )
-	#print( "vis_points length: {}".format( len( vis_points )) )
-
-	#print( "Cell data: {}".format( paraview_mesh.cell_data[cell_type][data_label] ) )
-	#print( "Cell data length: {}".format(  len(paraview_mesh.cell_data[cell_type][data_label]) ) )
-
-	#print( args )
-	tree = ET.parse( args.pvd_filename )
-	#print( tree )
-
-	root = tree.getroot()
-	#print( root )
-
-	#print( [elem.tag for elem in root.iter()] )
-	for dataset in root.iter("DataSet"):
-		#print( dataset )
-		#print( dataset.attrib )
-		print( dataset.attrib["file"] )
-	
-
-
-	#print( "Field data: {}".format( paraview_mesh.field_data ) )
-	#print( "Field data: {}".format( field_data ) )
-	#for fd in paraview_mesh.field_data:
-	#	print( "Field data: {}".format( fd) )
-
-	### Create preCICE interface
-	#interface = precice.Interface(participant_name, configuration_file_name, 0, 1)
+	### Create preCICE interfacemesh_id
+	interface = precice.Interface(participant_name, configuration_file_name, 0, 1)
 
 	#dimensions = interface.get_dimensions()
 	dimensions = 3
 
 	### Get mesh ID for preCICE
-	#mesh_id = interface.get_mesh_id(mesh_name)
+	my_mesh.mesh_id = interface.get_mesh_id( my_mesh.mesh_name )
 
 
 	#logging.basicConfig(level=getattr(logging, args.logging))
@@ -217,7 +171,7 @@ def main():
 	#print("Point data: ", mesh.celldata)
 	#n = len(mesh.points)
 
-	vertices_mesh = np.zeros((n, dimensions))
+	#vertices_mesh = np.zeros( )
 	pressure = []
 	concentration = []
 	
@@ -274,6 +228,7 @@ def parse_args():
                                      "Read meshes, partition them and write them out in internal format.")
 	#parser.add_argument("in_meshname", nargs="+", help="The meshes used as input")
 	parser.add_argument("pvd_filename", type=str, help="Path to ParaView PVD file to parse")
+	parser.add_argument("precice_config", type=str, help="Path to preCICE XML configuration")
 	parser.add_argument("--tag", "-t", dest="tag", default=None,
 		help="The CellData tag for vtk meshes")
 	parser.add_argument("--log", "-l", dest="logging", default="INFO", 
